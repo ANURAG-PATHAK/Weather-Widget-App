2b29ef8ed1782ba890a6f21ce9cb75ddb19b4589
ANURAG-PATHAK <pathakanurag605@gmail.com>
2023-07-07 03:38:52 +0530

    First Committ

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const apiKey = "370ce3f50efb664a0134ae4bee336ecb";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unit=metric&q=";
+const searchInput = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+        document.querySelector(".city").innerText = data.name;
+        document.querySelector(".temp").innerText = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerText = data.main.humidity + "%";
+        document.querySelector(".wind").innerText = data.wind.speed + "km/h";
+        switch (data.weather[0].main) {
+            case "Clear":
+                weatherIcon.src = "images/clear.png";
+                break;
+            case "Clouds":
+                weatherIcon.src = "images/clouds.png";
+                break;
+            case "Rain":
+                weatherIcon.src = "images/rain.png";
+                break;
+            case "Snow":
+                weatherIcon.src = "images/snow.png";
+                break;
+            case "Drizzle":
+                weatherIcon.src = "images/drizzle.png";
+                break;
+            case "Mist":
+                weatherIcon.src = "images/mist.png";
+                break;
+        }
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+searchButton.addEventListener("click", () => {
+    const city = searchInput.value;
+    checkWeather(city);
+});
+
+
+
+
+
